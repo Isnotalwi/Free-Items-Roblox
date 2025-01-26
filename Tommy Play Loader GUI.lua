c2dd13bcae8223f4c7775bc36e6a1dd99d2d5005
Isnotalwi <hilosarry7@gmail.com>
2025-01-26 13:31:54 +0700

    Update Tommy Play Loader GUI.lua

--- Tommy Play Loader GUI.lua
+++ Tommy Play Loader GUI.lua
@@ -1,6 +1,5 @@
-
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Imalwibest/refs/heads/main/Fluxus%20Ui.lib"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Isnotalwi/Roblox-Script/refs/heads/main/webhookV2.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Imalwibest/refs/heads/main/Fluxus%20Ui.lib"))()
 local items = {
     "Blue TJ New York Hoodie",
     "Chicago Red",
